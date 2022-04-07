8e95b7dfbdf5d6218e03c4c09ee04180ce8e693a
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-07 14:22:03 +0200

    test

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from flask import escape
-import functions_framework
-@functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
+def hello_world(request):
+    """Responds to any HTTP request.
     Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+        request (flask.Request): HTTP request object.
     Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
     else:
-        name = 'World'
-    return 'Hello {}!'.format(escape(name))
+        return f'Hello World!'
