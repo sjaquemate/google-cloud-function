ba5f7bc20299b84f270206d0f21f125a67d72133
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-07 14:07:38 +0200

    test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def hello_world():
-    return {'hello my world!'}
-
-def main(): 
-    return {'return from main()'}
-
-if __name__ == "__main__": 
-    main()
+def hello_world(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    return 'Hello world'
