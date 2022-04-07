fe4798d908cb7db18dcdd54d6fee5845d4014f0e
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-07 14:19:23 +0200

    test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from flask import escape
 import functions_framework
-
 @functions_framework.http
 def hello_http(request):
     """HTTP Cloud Function.
@@ -11,10 +11,13 @@ def hello_http(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
+    request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_args and 'name' in request_args:
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
         name = 'World'
-    return f'Hello {name}!'
+    return 'Hello {}!'.format(escape(name))
